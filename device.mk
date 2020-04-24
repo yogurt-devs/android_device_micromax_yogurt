24440835dee9aa8a7a0526d831a0371ea4b05ce8
Roshan Pius <rpius@google.com>
2020-04-24 14:33:28 -0700

    yogurt: rro_overlays: Add WifiOverlay
    Bug: 148617260
    Test: Device boots up and connects to wifi networks.
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Zinadin Zidan <zidan44@pixelexperience.org>
    Change-Id: If6a7a5eec116ea8d058d612f65ac434e36375246

--- device.mk
+++ device.mk
@@ -191,6 +191,7 @@ PRODUCT_PACKAGES += \
 
 # WiFi
 PRODUCT_PACKAGES += \
+    WifiOverlay \
     TetheringConfigOverlay \
     android.hardware.wifi@1.0.vendor \
     android.hardware.wifi@1.1.vendor \
