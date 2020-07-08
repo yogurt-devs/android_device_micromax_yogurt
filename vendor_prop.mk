13d05a1054a452c8d9fa234ba29948c9d8f2b482
Kshitij Gupta <kshitijgm@gmail.com>
2020-07-08 18:58:54 +0530

    yogurt: sf: Enable running_without_sync_framework
    - Fixes tint flickers in night-light
    
    Change-Id: Ic349e855c230ceb15303379a4a6a36d0b4a6de72
    Signed-off-by: SakthivelNadar <s2234nadar@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -695,6 +695,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # SurfaceFlinger
 PRODUCT_PROPERTY_OVERRIDES += \
+    ro.surface_flinger.running_without_sync_framework=true \
     ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
     ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
     ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000
