690e3d0c3787ce797a60f3bfff6daf06d1d91b1f
TTTT555 <albukerko@gmail.com>
2020-11-23 02:16:14 +0600

    E7746: Add camera aux package list
    Signed-off-by: SakthivelNadar <s2234nadar@gmail.com>

--- system_prop.mk
+++ system_prop.mk
@@ -22,7 +22,9 @@ PRODUCT_PRODUCT_PROPERTIES += \
 # Camera
 PRODUCT_PRODUCT_PROPERTIES += \
     persist.camera.HAL3.enabled=1 \
-    persist.vendor.camera.privapp.list=org.codeaurora.snapcam,com.android.camera,com.google.camera
+    persist.vendor.camera.privapp.list=org.codeaurora.snapcam,com.android.camera,com.google.camera \
+    vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,com.google.camera,org.pixelexperience.faceunlock \
+    vendor.camera.aux.packageblacklist=org.telegram.messenger,com.microsoft.teams,com.discord
 
 # WFD
 PRODUCT_PRODUCT_PROPERTIES += \
