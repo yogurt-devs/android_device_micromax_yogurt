b979ec9cd0a4466dd1ee267decbf69b3b078c39b
SakthivelNadar <s2234nadar@gmail.com>
2022-07-19 13:54:54 +0530

    E7746: add MTK perf props from stock
    Signed-off-by: SakthivelNadar <s2234nadar@gmail.com>

--- system_prop.mk
+++ system_prop.mk
@@ -2,3 +2,8 @@
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.sf.lcd_density=480
 
+# MTK Performance
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.mtk_perf_simple_start_win=1 \
+    ro.mtk_perf_fast_start_win=1 \
+    ro.mtk_perf_response_time=1
