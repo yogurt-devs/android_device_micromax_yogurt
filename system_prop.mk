bb312a644f7adcf411201fb424b8bfec5693f0c0
SakthivelNadar <s2234nadar@gmail.com>
2022-07-19 15:52:15 +0530

    E7746: import some missing props from stock
    Signed-off-by: SakthivelNadar <s2234nadar@gmail.com>

--- system_prop.mk
+++ system_prop.mk
@@ -1,6 +1,8 @@
 # Display
 PRODUCT_PRODUCT_PROPERTIES += \
-    ro.sf.lcd_density=480
+    ro.sf.lcd_density=480 \
+    qemu.hw.mainkeys=0 \
+    ro.opengles.version=196610
 
 # MTK Performance
 PRODUCT_PRODUCT_PROPERTIES += \
@@ -21,3 +23,28 @@ PRODUCT_PRODUCT_PROPERTIES += \
     persist.sys.wfd.nohdcp=1 \
     persist.debug.wfd.enable=1 \
     persist.sys.wfd.virtual=0
+
+# Kernel
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.kernel.zio=38,108,105,16
+
+# Wifi
+PRODUCT_PRODUCT_PROPERTIES += \
+    wifi.tethering.interface=ap0 \
+    wifi.direct.interface=p2p0 \
+    wifi.interface=wlan0 \
+    ro.mediatek.wlan.wsc=1 \
+    ro.mediatek.wlan.p2p=1 \
+    mediatek.wlan.ctia=0
+
+# Audio
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.camera.sound.forced=0 \
+    ro.audio.silent=0
+
+# USB
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.sys.usb.storage.type=mtp \
+    ro.sys.usb.mtp.whql.enable=0 \
+    ro.sys.usb.charging.only=yes \
+    ro.sys.usb.bicr=no
