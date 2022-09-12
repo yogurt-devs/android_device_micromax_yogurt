8b425d49de0a94ec1fe8346a136e5c8541ca7cd9
weichinweng <weichinweng@google.com>
2022-09-12 17:57:06 +0530

    yogurt: Align ASHA prop with BluetoothProperties * Modify bluetooth.profile.asha.central to   bluetooth.profile.asha.central.enabled
    Tag: #feature
    Test: manual
    Bug: 211570675
    Bug: 218454632
    Change-Id: I8e1f49034e5a675b29fba747e634c9f7c6c29c50
    
    Signed-off-by: neilchetty <neilchetty4559@gmail.com>
    Signed-off-by: SakthivelNadar <s2234nadar@gmail.com>

--- system_prop.mk
+++ system_prop.mk
@@ -98,4 +98,4 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 # Bluetooth
 PRODUCT_PRODUCT_PROPERTIES += \
     bluetooth.device.class_of_device=90,2,12 \
-    bluetooth.profile.asha.central=false
+    bluetooth.profile.asha.central.enabled=false
