adba122a7097fb28207ac05a04fd7214cd898f5c
Bruno Martins <bgcngm@gmail.com>
2022-09-14 14:51:15 +0100

    yogurt: Migrate to AIDL ClearKey DRM HAL
    Change-Id: Iad5cc1176978137c647069197b33f49fb4d98ab2
    Signed-off-by: SakthivelNadar <s2234nadar@gmail.com>

--- device.mk
+++ device.mk
@@ -201,7 +201,8 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.0.vendor \
     android.hardware.drm@1.1.vendor \
     android.hardware.drm@1.2.vendor \
-    android.hardware.drm@1.3.vendor
+    android.hardware.drm@1.3.vendor \
+    android.hardware.drm-service.clearkey \
 
 # WiFi
 PRODUCT_PACKAGES += \
