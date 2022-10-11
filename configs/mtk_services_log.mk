ea199639ec8c4d1cb8bec60f115f46b594a1b75b
bengis32 <bengris32@protonmail.ch>
2022-10-11 17:17:28 +0530

    yogurt: props: Silence gralloc4 logspam
    * Since Android 13, gralloc has been spamming the logs with:
    E gralloc4: Empty SMPTE 2094-40 data
    
    Signed-off-by: bengis32 <bengris32@protonmail.ch>
    Signed-off-by: neilchetty <neilchetty4559@gmail.com>
    Signed-off-by: SakthivelNadar <s2234nadar@gmail.com>

--- configs/mtk_services_log.mk
+++ configs/mtk_services_log.mk
@@ -278,3 +278,4 @@ PRODUCT_PRODUCT_PROPERTIES += \
     persist.log.tag.MDP=$(VENDOR_LOG_LEVEL) \
     persist.log.tag.ccci_mdinit=$(VENDOR_LOG_LEVEL) \
     persist.log.tag.thermal_repeater=$(VENDOR_LOG_LEVEL) \
+    persist.log.tag.gralloc4=$(VENDOR_LOG_LEVEL) \
