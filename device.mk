f85288ba76ce993d5117ca2b875c36a0fd3f4f84
bengris32 <bengris32@protonmail.ch>
2022-10-26 18:57:39 +0100

    yogurt: Switch to a basic vibrator service
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Signed-off-by: SakthivelNadar <s2234nadar@gmail.com>

--- device.mk
+++ device.mk
@@ -275,8 +275,9 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-	android.hardware.vibrator@1.0-impl \
-	android.hardware.vibrator@1.0-service
+    android.hardware.vibrator@1.0-impl \
+    android.hardware.vibrator@1.0-service \
+    android.hardware.vibrator-service.yogurt
 
 # VNDK
 PRODUCT_COPY_FILES += \
