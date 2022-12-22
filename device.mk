d3742426cdc5433e884ad2bafa87c0abeb3df0bf
SakthivelNadar <s2234nadar@gmail.com>
2022-12-22 11:28:53 +0530

    yogurt: initial skeleton tree
    Signed-off-by: SakthivelNadar <s2234nadar@gmail.com>

---
+++ device.mk
@@ -0,0 +1,21 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
+# Include GSI keys
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# Define Dynamic Partition support
+PRODUCT_TARGET_VNDK_VERSION := 29
+PRODUCT_SHIPPING_API_LEVEL := 29
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+PRODUCT_BUILD_SUPER_PARTITION := false
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
