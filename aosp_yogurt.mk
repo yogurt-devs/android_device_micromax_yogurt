73cb8392cfe645edd067c2164325d23ac8956025
SakthivelNadar <s2234nadar@gmail.com>
2023-03-13 16:00:30 +0530

    yogurt: bringup PixelOS.
    Signed-off-by: SakthivelNadar <s2234nadar@gmail.com>

--- aosp_yogurt.mk
+++ aosp_yogurt.mk
@@ -8,14 +8,14 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common PixelOS stuff.
+$(call inherit-product, vendor/aosp/config/common.mk)
 
 # Inherit from E7746 device
 $(call inherit-product, device/micromax/yogurt/device.mk)
 
 PRODUCT_DEVICE := yogurt
-PRODUCT_NAME := lineage_yogurt
+PRODUCT_NAME := aosp_yogurt
 PRODUCT_BRAND := Micromax
 PRODUCT_MODEL := IN_Note1
 PRODUCT_MANUFACTURER := micromax
