2b97374d169ce60d425ac7d84a9bd6db5d604a5e
SakthivelNadar <s2234nadar@gmail.com>
2023-09-07 17:05:00 +0530

    yogurt: Update DRM blobs from `OMIX_X600-NFC`
    - OMIX/X600_NFC/X600_NFC:12/SP1A.210812.016/1658802762:user/release-keys
    
    Signed-off-by: SakthivelNadar <s2234nadar@gmail.com>

--- device.mk
+++ device.mk
@@ -188,7 +188,10 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.3.vendor \
+    libmockdrmcryptoplugin \
+
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.4.vendor \
     android.hardware.drm-service.clearkey \
 
 # WiFi
